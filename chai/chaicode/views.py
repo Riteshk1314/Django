391329a4a91f1d13346f76923e3ab99115764e4f
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-06-01 21:38:38 +0530

    making forms

--- chai/chaicode/views.py
+++ chai/chaicode/views.py
@@ -10,3 +10,7 @@ from django.shortcuts import render, get_object_or_404
 def chai_detail(request, chai_id):
   chai = get_object_or_404(ChaiVariety, pk=chai_id)
   return render(request, 'website/chai_detail.html', {'chai': chai})
+
+
+def chai_store_view(request):
+    return render(request, 'website/chai_stores.html')
